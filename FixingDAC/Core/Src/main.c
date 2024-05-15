4aaeb85dc3a61cf2d97086828ed62de13b5e6815
Jared Morrison <jrdmrrsn@gmail.com>
2024-05-15 11:28:36 -0400

    Update TIM1 prescaler and ARR

--- FixingDAC/Core/Src/main.c
+++ FixingDAC/Core/Src/main.c
@@ -1240,9 +1240,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 100-1;
+  htim1.Init.Prescaler = 96-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 60000-1;
+  htim1.Init.Period = 62500-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
