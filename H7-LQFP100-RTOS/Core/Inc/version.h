f852c9b239769a452b5872f19044e50719549152
Anthony Santos <Anthony.Santos@unh.edu>
2025-01-23 13:04:30 -0500

    Commit hash complete, removing testfile.. Commit hashes can only be retrieved from this commit onward

--- H7-LQFP100-RTOS/Core/Inc/version.h
+++ H7-LQFP100-RTOS/Core/Inc/version.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[01378d123813ccb4964534e9073d5756041831f4, (HEAD -> packet-restructure, origin/packet-restructure)]\r\n";
+ static const char* GIT_INFO = "Version Information=[0b83fc1b0831cd9dd18ddbd5eef2c39b5b4198e4, (HEAD -> packet-restructure, origin/packet-restructure)]\r\n";
