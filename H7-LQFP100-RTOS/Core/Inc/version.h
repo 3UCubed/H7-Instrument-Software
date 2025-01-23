01378d123813ccb4964534e9073d5756041831f4
Anthony Santos <Anthony.Santos@unh.edu>
2025-01-23 12:34:25 -0500

    Using pre-build instead of pre-push
    making a pre-push hook to have the current commit hash added to the actual current commit hash will never work unless we have outside dependencies. Using pre-build instead, as it's included directly in STM32CubeIIDE. And when we build, we are grabbing the current commit. So, the current commit just won't be reflected upon pushing.

--- H7-LQFP100-RTOS/Core/Inc/version.h
+++ H7-LQFP100-RTOS/Core/Inc/version.h
@@ -1,1 +1,2 @@
-#define FIRMWARE_VERSION "1cbb914"
+#define GIT_INFO_PRESENT
+ static const char* GIT_INFO = "Version Information=[1081baf6c1d2d2d4abfb1e4edd2fc554ffa2dd11, (HEAD -> packet-restructure, origin/packet-restructure)]\r\n";
