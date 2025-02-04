acc5ffcaa9d9020f9c9f9413f2aa6d85a800ae69
Anthony Santos <Anthony.Santos@unh.edu>
2025-02-04 12:29:32 -0500

    fixed pre-commit hook, auto version numbering should work now

--- H7-LQFP100-RTOS/Core/Inc/version.h
+++ H7-LQFP100-RTOS/Core/Inc/version.h
@@ -1,10 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// Automatically updated by the pre-commit hook
-
 #define V_MAJOR 1
 #define V_MINOR 0
-#define V_PATCH 0
+#define V_PATCH 1
 
 #endif // VERSION_H
